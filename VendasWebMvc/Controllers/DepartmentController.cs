88d0343bb07bf8ba3b02f0ed3a533c6f935cbc43
Carlos-Eduardo-Kadu <kadumeloduarte@gmail.com>
2025-06-30 15:59:07 -0300

    primeira model view controller Departamento

---
+++ VendasWebMvc/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VendasWebMvc.Models;
+
+namespace VendasWebMvc.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List < Department > list = new List<Department> ();
+            list.Add(new Department { Id = 1, Name = "Eletronic" });
+            list.Add(new Department { Id = 2, Name = "Moda" });
+            return View(list);
+        }
+    }
+}
