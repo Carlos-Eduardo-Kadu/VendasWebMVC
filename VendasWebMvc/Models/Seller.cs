2bb56c07a629368d1fb2c843593cc61d2a7dd7b3
Carlos-Eduardo-Kadu <kadumeloduarte@gmail.com>
2025-07-20 10:35:07 -0300

    Sellers Controller

--- VendasWebMvc/Models/Seller.cs
+++ VendasWebMvc/Models/Seller.cs
@@ -7,7 +7,7 @@ namespace VendasWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime birthDtae { get; set; }
+        public DateTime BirthDtae { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -22,7 +22,7 @@ namespace VendasWebMvc.Models
             Id = id;
             Name = name;
             Email = email;
-            this.birthDtae = birthDtae;
+            BirthDtae = birthDtae;
             BaseSalary = baseSalary;
             Department = department;
         }
